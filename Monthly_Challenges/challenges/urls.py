ff000776c46cd2d880e20501d2ec1ce8bb4a06ee
Cassius <cassiusoliveira10@gmail.com>
2023-08-01 17:20:15 -0400

    Implemented dynamic urls for home and challenge template

--- Monthly_Challenges/challenges/urls.py
+++ Monthly_Challenges/challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="view-challenges"),
+    path("", views.home, name="home"),
     path("<int:month>", views.month_by_number),
     path("<str:month>", views.monthly_challenge, name="month-challenge"),
 ]
